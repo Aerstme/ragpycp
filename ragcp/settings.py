30ad33dbc74e3cf067cd40098f7dffcacd58630c
Giuliano P. Tognarelli Buono-core <gtognarelli@linux.com>
2019-05-18 21:56:10 -0400

    Added background task that download all item images and portraits from rune nifelheim

--- ragcp/settings.py
+++ ragcp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'char'
+    'char',
+    'background_task',
 ]
 
 AUTH_USER_MODEL = 'users.Login'
