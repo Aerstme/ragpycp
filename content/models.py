e377e2964385b4f128f36a85f7fbc5a3d777b0bf
Giuliano P. Tognarelli Buono-core <gtognarelli@linux.com>
2019-09-11 11:12:40 -0300

    Added post model

--- content/models.py
+++ content/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from users.models import Login
+
+
+class Post(models.Model):
+    class Post(models.Model):
+        created_by = models.ForeignKey(Login, on_delete=models.CASCADE)
+        created_at = models.DateTimeField('Creation date', auto_now=True)
+        body = models.CharField(max_length=1024, null=False, default=None, verbose_name='Content')
